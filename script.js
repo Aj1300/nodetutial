4c51f968155ccf505f13fa3437818371fc7b9276
Aj1300 <antonyjees8@gmail.com>
2024-11-06 11:15:41 +0530

    read file

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const path=require('path')//its a module that get us path
 const fs=require('fs')
 const user="JeesAppan"
-fs.writeFile(path.join(__dirname,"/api2","api.txt"),`User name:${user}`,(err)=>{
+fs.readFile(path.join(__dirname,"/api2","api.txt"),"utf8",(err,data)=>{
     if(err) throw err;
-})
+    console.log(data)
+});
 
 
   
